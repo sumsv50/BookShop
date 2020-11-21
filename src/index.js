67857bc9a71f288c0c336ef353efc2a0ec514d06
Sum Duong <sumsvpro@gmail.com>
2020-11-21 10:26:50 +0700

    Fix bug 1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 const route = require('./routes');
 
